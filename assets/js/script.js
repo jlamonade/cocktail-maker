75b3aa3103b1a3a1cb863dbab1c4e48f39ac1383
Jason Lam <nyjasonlam@gmail.com>
2021-04-28 11:07:41 -0400

    Added validateIngredintsInput()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 // DEPENDENCIES
-var ingredientsDiv = document.querySelector(".ingredients-div")
+var ingredientsDiv = document.querySelector(".ingredients-div");
+var ingredientInput = document.querySelector(".ingredient-input");
 
 // STARTING DATA
-var temporaryIngredientsArray = []
+var temporaryIngredientsArray = [];
 
-function validateIngredientInput() {
-    return
+function validateIngredientInput(ingredient) {
+  var requestUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=";
+  var ingredients = ingredient;
+  fetch(requestUrl + ingredients)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.ingredients !== null) {
+        temporaryIngredientsArray.push(ingredient);
+      }
+    });
 }
+
+
+validateIngredientInput("gin");
+console.log(temporaryIngredientsArray);
