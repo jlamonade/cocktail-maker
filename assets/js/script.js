88bb49e759b729b98a810943e6aa97b00a3a8202
Danny Sollomoni <dsollomoni@gmail.com>
2021-04-28 12:10:34 -0400

    function for fetching

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var API_key = "1";
+var listdrinks = [];
+var firstLetter = "M";
+
+var getData = async() => {
+    var response = await fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`)
+    var data = await response.json()
+    console.log(data)
+}
+getData();
